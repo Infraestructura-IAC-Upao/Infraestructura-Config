3a18d80dbf8d08dc918aaf30b6c6dea705558fdf
javierUPAO <jaguilarb3@upao.edu.pe>
2025-05-06 00:33:22 -0500

    fix:Corrigiendo el output y agregando la variable eip_ip que es el valor de la ip elastica

--- terraform/Efimero/variables.tf
+++ terraform/Efimero/variables.tf
@@ -15,6 +15,7 @@ variable "aws_region" {
 locals {
   vpc_id  = data.terraform_remote_state.Persistente.outputs.VpcIP
   eip     = data.terraform_remote_state.Persistente.outputs.elastic_ip_allocation_id
+  eip_ip  = data.terraform_remote_state.Persistente.outputs.elasticIP
   subnet_2a = data.terraform_remote_state.Persistente.outputs.aws_subnet_public_subnet-us-est-2a
   subnet_2b = data.terraform_remote_state.Persistente.outputs.aws_subnet_public_subnet-us-est-2b
 }
