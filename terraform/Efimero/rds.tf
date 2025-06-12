bedd84ebd66476e8f026641d4bf155ca93f699df
Ginnn13 <gino_josue_13@hotmail.com>
2025-06-11 19:23:13 -0500

    fix: [NONE][CKV_AWS_382] Ensure no security groups allow egress from 0.0.0.0:0 to port -1 /Efimero/rds.tf.aws_security_group.rds_sg estricción de IPs: Se limita el tráfico saliente a un rango específico, como 10.0.0.0/16. Restricción de puertos: Solo se permite el tráfico en el puerto 443 (HTTPS). Descripción: Se añade una descripción clara para explicar el propósito de la regla.

--- terraform/Efimero/rds.tf
+++ terraform/Efimero/rds.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "rds_sg" {
   name        = "rds_sg"
   description = "Allow DB access from backend EC2"
   vpc_id = "${local.vpc_id}"
-  
+
   ingress {
     from_port       = 5432
     to_port         = 5432
@@ -31,10 +31,10 @@ resource "aws_security_group" "rds_sg" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
     description = "Allow all outbound traffic"
   }
 }
