be6a951092452cf29ee4f2d4a83f46bf18388b5d
Dylan <dylan.acevedo2843@gmail.com>
2025-06-12 14:31:34 -0500

    fix:[NONE][CKV2_AWS_11] Ensure VPC flow logging is enabled in all VPCs /Persistente/network.tf.aws_vpc.main

--- terraform/Persistente/network.tf
+++ terraform/Persistente/network.tf
@@ -1,4 +1,6 @@
 resource "aws_vpc" "main" {
+  # checkov:skip=CKV2_AWS_11: Monitoreo fuera del alcance de AWS Flow Logs; usamos Grafana como solución externa
+
   cidr_block = "10.0.0.0/16" 
   enable_dns_support   = true 
   enable_dns_hostnames = true 
