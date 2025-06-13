a7b32223d1d8bd134900a3ffeedca285b6d0e3a5
javierUPAO <jaguilarb3@upao.edu.pe>
2025-06-13 01:39:19 -0500

    fix:comentario para bypasear algunas alertas

--- terraform/Persistente/cloud-front.tf
+++ terraform/Persistente/cloud-front.tf
@@ -2,8 +2,9 @@ resource "aws_cloudfront_origin_access_identity" "frontend_oai" {
   comment = "Access for CloudFront to bere-frontend"
 }
 
-# checkov:skip=CKV2-AWS-47: No se usa WAF en esta distribución; protección externa se maneja por otros medios
+
 resource "aws_cloudfront_distribution" "frontend_cf" {
+  # checkov:skip=CKV2-AWS-47: No se usa WAF en esta distribución; protección externa se maneja por otros medios
   # checkov:skip=CKV_AWS_310: No se requiere failover en esta distribución, es de un solo origen
   # checkov:skip=CKV_AWS_174: Se utiliza certificado por defecto de CloudFront, sin opción a forzar TLS v1.2, ACM genera costos
   # checkov:skip=CKV_AWS_68: No se requiere WAF, es una distribución básica sin exposición crítica ni necesidad de costos adicionales
