9b455c20667deae9c99a9281db21b16eda51e37f
rpc05 <jpalominoc5@upao.edu.pe>
2025-06-12 21:41:25 -0500

    skip: [NONE][CKV_AWS_86] Ensure CloudFront distribution has Access Logging enabled /Persistente/cloud-front.tf.aws_cloudfront_distribution.frontend_cf

--- terraform/Efimero/rds.tf
+++ terraform/Efimero/rds.tf
@@ -40,6 +40,7 @@ resource "aws_security_group" "rds_sg" {
 }
 # checkov:skip=CKV_AWS_118 reason=No se requiere monitoreo mejorado en este entorno
 # checkov:skip=CKV_AWS_157 reason=Entorno no requiere alta disponibilidad
+# checkov:skip=CKV_AWS_86 reason=No se requiere logging en entorno no productivo
 resource "aws_db_instance" "bere_db" {
   identifier                        = "bere-db"
   engine                            = "postgres"
